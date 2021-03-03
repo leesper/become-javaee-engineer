d3ef2a08ab7eedccb9fb2978f9b1002cc3fc7f86
leesper <pascal7718@gmail.com>
2021-03-03 14:51:01 +0800

    feat: Add course controller

---
+++ kkb_mm/mm_oms/src/main/java/com/kkb/controller/CourseController.java
@@ -0,0 +1,45 @@
+package com.kkb.controller;
+
+import com.kkb.domain.AjaxResult;
+import com.kkb.pojo.TCourse;
+import com.kkb.pojo.TUser;
+import com.kkb.service.CourseService;
+import com.kkb.utils.ServletUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+import static com.kkb.constant.GlobalConst.SESSION_KEY_USER;
+
+@Controller
+public class CourseController extends BaseController {
+    @Autowired
+    private CourseService courseService;
+
+    @PostMapping("course/add")
+    @ResponseBody
+    public AjaxResult addCourse(@Validated @RequestBody TCourse course) {
+        try {
+            course.setCreateDate(new Date());
+
+            TUser user = getSessionUser(SESSION_KEY_USER);
+            if (user != null) {
+                logger.info("current user: " + user.toString());
+                course.setUserId(user.getId());
+            } else {
+                course.setUserId(1);
+            }
+
+            int rows = courseService.addCourse(course);
+            return toAjax(rows);
+        } catch (Exception e) {
+            logger.error("add course error: " + e.getMessage());
+            return AjaxResult.error(e.getMessage());
+        }
+    }
+}
